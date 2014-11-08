b3a9bc9150b07c181545e4d453f2e531cdaf9246
Darius <team.duel@gmail.com>
2014-11-08 11:47:57 +0200

    facebook login

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new NfqAkademija\FrontendBundle\NfqAkademijaFrontendBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
 
         );
 
